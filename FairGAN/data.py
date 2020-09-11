584b27089042e1839eeabd35bce48931730c9142
Bearwithchris <Christopher_teo@mymail.sutd.edu.sg>
2020-09-11 11:25:52 +0800

    comment removed

--- FairGAN/data.py
+++ FairGAN/data.py
@@ -152,83 +152,83 @@ def datasets(dir1,dir2,samples,bias,gamma=1.0,evaluation=True):
 
 
 #=========Prep Data Reduce Load==============================================================================================
-# def normalize(image):
-#     '''
-#         normalizing the images to [-1, 1]
-#     '''
-#     image = tf.cast(image, tf.float32) 
-#     # image = tf.cast(image, tf.float16) #Trying mix precision
-#     image = (image - 127.5) / 127.5
-#     return image
+def normalize(image):
+    '''
+        normalizing the images to [-1, 1]
+    '''
+    image = tf.cast(image, tf.float32) 
+    # image = tf.cast(image, tf.float16) #Trying mix precision
+    image = (image - 127.5) / 127.5
+    return image
 
-# def augmentation(image):
-#     '''
-#         Perform some augmentation
-#     '''
-#     image = tf.image.random_flip_left_right(image)
-#     return image
+def augmentation(image):
+    '''
+        Perform some augmentation
+    '''
+    image = tf.image.random_flip_left_right(image)
+    return image
 
-# def preprocess_image(file_path, target_size=512):
-#     images = tf.io.read_file(file_path)
-#     # convert the compressed string to a 3D uint8 tensor
-#     images = tf.image.decode_jpeg(images, channels=3)
-#     images = tf.image.resize(images, (target_size, target_size),
-#                             method='nearest', antialias=True)
-#     images = augmentation(images)
-#     images = normalize(images)
-#     return images
+def preprocess_image(file_path, target_size=512):
+    images = tf.io.read_file(file_path)
+    # convert the compressed string to a 3D uint8 tensor
+    images = tf.image.decode_jpeg(images, channels=3)
+    images = tf.image.resize(images, (target_size, target_size),
+                            method='nearest', antialias=True)
+    images = augmentation(images)
+    images = normalize(images)
+    return images
 
-# def loadImgs_random_V2(dir_images,target_size=32,cap=1000):
-#     list_ds=tf.data.Dataset.list_files(dir_images + '/*') 
-#     preprocess_function = partial(preprocess_image, target_size=32)  #Partially fill in a function data.preprocess_image with the arguement image_size
-#     list_ds=list_ds.take(cap)
-#     # train_data = list_ds.map(preprocess_function).shuffle(100)  #Apply the function pre_process to list_ds
-#     train_data = list_ds.map(preprocess_function) #Apply the function pre_process to list_ds
+def loadImgs_random_V2(dir_images,target_size=32,cap=1000):
+    list_ds=tf.data.Dataset.list_files(dir_images + '/*') 
+    preprocess_function = partial(preprocess_image, target_size=32)  #Partially fill in a function data.preprocess_image with the arguement image_size
+    list_ds=list_ds.take(cap)
+    # train_data = list_ds.map(preprocess_function).shuffle(100)  #Apply the function pre_process to list_ds
+    train_data = list_ds.map(preprocess_function) #Apply the function pre_process to list_ds
 
-#     return train_data #Return back a data_set
+    return train_data #Return back a data_set
 
-# def prep_bias_data_V2(directory_male,directory_female,samples,bias=0.5):
-#     female_samples=samples*bias
-#     male_samples=samples*(1.0-bias)
+def prep_bias_data_V2(directory_male,directory_female,samples,bias=0.5):
+    female_samples=samples*bias
+    male_samples=samples*(1.0-bias)
     
-#     #Load_bias_data
-#     images_male=loadImgs_random_V2(directory_male,male_samples)
-#     images_female=loadImgs_random_V2(directory_female,female_samples)
+    #Load_bias_data
+    images_male=loadImgs_random_V2(directory_male,male_samples)
+    images_female=loadImgs_random_V2(directory_female,female_samples)
     
-#     images_concat=images_male.concatenate(images_female)
-#     # images_concat=images_concat.shuffle(100)
+    images_concat=images_male.concatenate(images_female)
+    # images_concat=images_concat.shuffle(100)
     
-#     return images_concat
+    return images_concat
 
-# def datasets_V2(dir1,dir2,samples,bias,gamma=1.0,evaluation=True):
-#     bias_data_samples=samples/(gamma+1)
-#     ref_data_samples=samples-bias_data_samples
+def datasets_V2(dir1,dir2,samples,bias,gamma=1.0,evaluation=True):
+    bias_data_samples=samples/(gamma+1)
+    ref_data_samples=samples-bias_data_samples
     
-#     bias=prep_bias_data_V2(dir1,dir2,bias_data_samples,bias)
-#     bias_labels=tf.data.Dataset.from_tensors(np.ones(int(bias_data_samples)))
+    bias=prep_bias_data_V2(dir1,dir2,bias_data_samples,bias)
+    bias_labels=tf.data.Dataset.from_tensors(np.ones(int(bias_data_samples)))
     
-#     ref=prep_bias_data_V2(dir1,dir2,ref_data_samples)
-#     ref_labels=tf.data.Dataset.from_tensors(np.zeros(int(ref_data_samples)))
+    ref=prep_bias_data_V2(dir1,dir2,ref_data_samples)
+    ref_labels=tf.data.Dataset.from_tensors(np.zeros(int(ref_data_samples)))
     
     
-#     if evaluation==False:
-#         images_labels_concat=ref_labels.concatenate(bias_labels)
-#         images_concat=ref.concatenate(bias)
+    if evaluation==False:
+        images_labels_concat=ref_labels.concatenate(bias_labels)
+        images_concat=ref.concatenate(bias)
         
-#         # images_concat,images_labels_concat=shuffle(images_concat,images_labels_concat)
-#     else:
-#         batch=10
+        # images_concat,images_labels_concat=shuffle(images_concat,images_labels_concat)
+    else:
+        batch=10
         
-#         bias=bias.batch(batch)
-#         bias_labels=bias_labels.batch(batch)
+        bias=bias.batch(batch)
+        bias_labels=bias_labels.batch(batch)
         
-#         ref=ref.batch(batch)
-#         ref_labels=ref_labels.batch(batch)
+        ref=ref.batch(batch)
+        ref_labels=ref_labels.batch(batch)
         
-#         images_concat=tf.data.Dataset.zip((ref, bias)).map(lambda x, y: tf.concat((x, y), axis=0))
-#         images_labels_concat=tf.data.Dataset.zip((ref_labels, bias_labels)).map(lambda x, y: tf.concat((x, y), axis=0))
+        images_concat=tf.data.Dataset.zip((ref, bias)).map(lambda x, y: tf.concat((x, y), axis=0))
+        images_labels_concat=tf.data.Dataset.zip((ref_labels, bias_labels)).map(lambda x, y: tf.concat((x, y), axis=0))
 
     
-#     return images_labels_concat,images_concat
+    return images_labels_concat,images_concat
 
 # labels,images=datasets_V2(dir1,dir2,100,bias=0.9)
